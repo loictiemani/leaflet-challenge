b37e7fa313832a8c01853b194bffea01655f6f65
Loic Tiemani <loictiems@github.com>
2021-02-20 15:16:09 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,3 +2,9 @@
 var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+  });
+  
