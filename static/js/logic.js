5693c285b9c1a2a08c4393de6f44fff776bf5ae3
Loic Tiemani <loictiems@github.com>
2021-02-20 17:41:43 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -40,7 +40,7 @@ function createMap(earthquakes) {
 
     // Define Variable and Tile layers
     var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      attribution: "Map data © <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
       tileSize: 512,
       maxZoom: 18,
       zoomOffset: -1,
