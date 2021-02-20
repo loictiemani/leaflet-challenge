64f0e7d9e3758c7bbe113ab74192a1f77c82347a
Loic Tiemani <loictiems@github.com>
2021-02-20 18:57:52 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,34 +10,10 @@ var earthquakes_layer = new L.LayerGroup();
 var tectonicPlates_layer = new L.LayerGroup();
 
 
-// Define Variables for Tile Layers
-
-
-
 // Perform a GET request to the query URL
 d3.json(EarthQuakeUrl, function(data) {
     // Once we get a response, send the data.features object to the createFeatures function
-    createFeatures(data.features);
-  });
   
-  function createFeatures(earthquakeData) {
-
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place and time of the earthquake
-    function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-    }
-   // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
- // Sending our earthquakes layer to the createMap function
- createMap(earthquakes);
-}
-function createMap(earthquakes) {
-
     // Define Variable and Tile layers
     var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data © <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -76,13 +52,13 @@ function createMap(earthquakes) {
       " Fault Lines": tectonicPlates_layer,
     };
   
-    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    // Create  map, giving it Satellitemap and earthquakes layers to display on load
     var myMap = L.map("mapid", {
       center: [
         37.09, -95.71
       ],
       zoom: 5,
-      layers: [streetmap, earthquakes]
+      layers: [satelliteMap, earthquakes_layer]
     });
   
     // Create a layer control
@@ -90,6 +66,24 @@ function createMap(earthquakes) {
     // Add the layer control to the map
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
-    }).addTo(myMap);
-  }
-  
+     }).addTo(myMap);
+
+     
+
+     // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
+     function chooseColor(magnitude) {
+        switch (true) {
+        case magnitude > 5:
+            return "#581845";
+        case magnitude > 4:
+            return "#900C3F";
+        case magnitude > 3:
+            return "#C70039";
+        case magnitude > 2:
+            return "#FF5733";
+        case magnitude > 1:
+            return "#FFC300";
+        default:
+            return "#DAF7A6";
+        }
+     }
