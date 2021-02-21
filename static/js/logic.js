d075a4549111a721590766537ae6ef01772d324e
Loic Tiemani <loictiems@github.com>
2021-02-20 22:26:56 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,15 +10,6 @@ var earthquakes_layer = new L.LayerGroup();
 var tectonicPlates_layer = new L.LayerGroup();
 
 
-// Perform a GET request to the query URL
-d3.json(EarthQuakeUrl, function(data) {
-    // Once we get a response, send the data.features object to the createFeatures function
-
-    var earthquakes = data.features
-    console.log(earthquakes)
-  // Loop through the earthquake data and create one circle marker for each earthquake
-
-  
     // Define Variable and Tile layers
     var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data © <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -73,10 +64,30 @@ d3.json(EarthQuakeUrl, function(data) {
       collapsed: false
      }).addTo(myMap);
 
-     
 
-     // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
-     function chooseColor(magnitude) {
+     // Retrieve earthquakesURL (USGS Earthquakes GeoJSON Data) with D3
+d3.json(EarthQuakeUrl, function(earthquakeData) {
+    // Function to Determine Size of Marker Based on the Magnitude of the Earthquake
+    function markerSize(magnitude) {
+        if (magnitude === 0) {
+          return 1;
+        }
+        return magnitude * 3;
+    }
+    // Function to Determine Style of Marker Based on the Magnitude of the Earthquake
+    function styleInfo(feature) {
+        return {
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: chooseColor(feature.properties.mag),
+          color: "#000000",
+          radius: markerSize(feature.properties.mag),
+          stroke: true,
+          weight: 0.5
+        };
+    }
+    // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
+    function chooseColor(magnitude) {
         switch (true) {
         case magnitude > 5:
             return "#581845";
@@ -91,4 +102,53 @@ d3.json(EarthQuakeUrl, function(data) {
         default:
             return "#DAF7A6";
         }
-     }
+    }
+    // Create a GeoJSON Layer Containing the Features Array on the earthquakeData Object
+    L.geoJSON(earthquakeData, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        // Function to Run Once For Each feature in the features Array
+        // Give Each feature a Popup Describing the Place & Time of the Earthquake
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h4>Location: " + feature.properties.place + 
+            "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
+            "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+        }
+    // Add earthquakeData to earthquakes LayerGroups 
+    }).addTo(earthquakes_layer);
+    // Add earthquakes Layer to the Map
+    earthquakes_layer.addTo(myMap);
+
+    // Retrieve platesURL (Tectonic Plates GeoJSON Data) with D3
+    d3.json(platesURL, function(plateData) {
+        // Create a GeoJSON Layer the plateData
+        L.geoJson(plateData, {
+            color: "#DC143C",
+            weight: 2
+        // Add plateData to tectonicPlates LayerGroups 
+        }).addTo(tectonicPlates_layer);
+        // Add tectonicPlates Layer to the Map
+        tectonicPlates_layer.addTo(myMap);
+    });
+
+    // Set Up Legend
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend"), 
+        magnitudeLevels = [0, 1, 2, 3, 4, 5];
+
+        div.innerHTML += "<h3>Magnitude</h3>"
+
+        for (var i = 0; i < magnitudeLevels.length; i++) {
+            div.innerHTML +=
+                '<i style="background: ' + chooseColor(magnitudeLevels[i] + 1) + '"></i> ' +
+                magnitudeLevels[i] + (magnitudeLevels[i + 1] ? '&ndash;' + magnitudeLevels[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    // Add Legend to the Map
+    legend.addTo(myMap);
+
+});
